f420b484f285a36609813ce573a159e3f574405f
KDK(NoteBook) <rkdqmffor@gmail.com>
2018-10-10 23:19:15 +0900

    make New NPC Line
    주인공이 이동후 끝에 새로운 npc줄이 생성되는 함수 작성

--- Project/main.py
+++ Project/main.py
@@ -76,26 +76,10 @@ class NPC:
         elif (self.direct == 3):
             self.image.clip_draw(self.direct * 64, 0, 64, 200, self.x, self.y)
 
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                hero.position += 1
-                hero.walk_mode=1
-            elif event.key == SDLK_LEFT:
-                hero.position -= 1
-                hero.walk_mode = 1
-            elif event.key == SDLK_ESCAPE:
-                running = False
-
-
 def make_NPCblank_Init():
     rand_num = 2
     npc[0][rand_num].direct = 5
+
     for i in range(1, 6):
         if rand_num == 0:
             rand_num = random.randint(0, 1)
@@ -109,17 +93,84 @@ def make_NPCblank_Init():
         elif rand_num == 3:
             rand_num = random.randint(2, 3)
             npc[i][rand_num].direct = 5  # Blank Position
-def draw_NPC():
+
     for i in range(5,-1,-1):
         for j in range(0,4):
             npc[i][j].x=100*j+250
             npc[i][j].y=100*i+100
+def draw_NPC():
+    for i in range(5,-1,-1):
+        for j in range(0,4):
             npc[i][j].draw()
+
+def make_NPC_New(rand_num):
+    for i in range(0, 4):
+        npc[5][i].form = random.randint(0, 8)
+        npc[5][i].direct=0
+    if rand_num == 0:
+        rand_num = random.randint(0, 1)
+        npc[5][rand_num].direct = 5  # Blank Position
+    elif rand_num == 1:
+        rand_num = random.randint(0, 2)
+        npc[5][rand_num].direct = 5  # Blank
+    elif rand_num == 2:
+        rand_num = random.randint(1, 3)
+        npc[5][rand_num].direct = 5  # Blank Position
+    elif rand_num == 3:
+        rand_num = random.randint(2, 3)
+        npc[5][rand_num].direct = 5  # Blank Position
+def move_NPC():
+    for i in range(5,-1,-1):  #move NPC y Position
+        for j in range(0,4):
+            npc[i][j].y-=100
+            if npc[i][j].y==0:
+                npc[i][j].y=600
+    for i in range(1,6):      #move NPC list position
+        npc[i-1], npc[i]=npc[i],npc[i-1]
+    if (npc[4][0].direct==5):
+        make_NPC_New(0)
+    elif (npc[4][1].direct==5):
+        make_NPC_New(1)
+    elif (npc[4][2].direct==5):
+        make_NPC_New(2)
+    elif (npc[4][3].direct==5):
+        make_NPC_New(3)
+
 def draw_hero():
     hero.x = 100 * hero.position + 250
     hero.y = 100
     hero.draw()
     hero.update()
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                hero.position += 1
+                if hero.position==4:
+                    hero.position = 3
+                else:
+                    hero.walk_mode=1
+                    move_NPC()
+            elif event.key == SDLK_LEFT:
+                hero.position -= 1
+                if hero.position==-1:
+                    hero.position = 0
+                else:
+                    hero.walk_mode = 1
+                    move_NPC()
+            elif event.key == SDLK_UP:
+                hero.walk_mode = 1
+                move_NPC()
+            elif event.key == SDLK_ESCAPE:
+                running = False
+
+
+
 #main
 hero=Hero()
 npc=[[NPC() for i in range(4)] for i in range(6)]
@@ -139,4 +190,4 @@ while running:
     #################################
     update_canvas()
     get_events()
-    delay(0.2)
+    delay(0.1)
