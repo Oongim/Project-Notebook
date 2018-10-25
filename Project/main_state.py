bae158f5da99132567e33e79f6205f87943732ed
KDK(NoteBook) <rkdqmffor@gmail.com>
2018-10-25 21:47:39 +0900

    카운트 표시 성공
    오른쪽에 카운트 이미지를 띄워서 카운트하기 성공

--- Project/main_state.py
+++ Project/main_state.py
@@ -7,7 +7,52 @@ from Hero import Hero
 from NPC import NPC
 from Map import Map
 
-
+class Count:
+    def __init__(self):
+        self.x=730
+        self.y=560
+        self.count=0
+        self.zero=load_image('Resource\Count\\0.png')
+        self.one = load_image('Resource\Count\\1.png')
+        self.two = load_image('Resource\Count\\2.png')
+        self.three = load_image('Resource\Count\\3.png')
+        self.four = load_image('Resource\Count\\4.png')
+        self.five = load_image('Resource\Count\\5.png')
+        self.six = load_image('Resource\Count\\6.png')
+        self.seven = load_image('Resource\Count\\7.png')
+        self.eight = load_image('Resource\Count\\8.png')
+        self.nine = load_image('Resource\Count\\9.png')
+    def update(self):
+        self.y -= 100
+        self.count+=1
+        if self.y <= 0:
+            self.y += 600
+    def draw(self):
+        count=self.count
+        posx=self.x
+        while(count!=0):
+            if (count % 10 == 0):
+                self.zero.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 1):
+                self.one.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 2):
+                self.two.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 3):
+                self.three.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 4):
+                self.four.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 5):
+                self.five.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 6):
+                self.six.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 7):
+                self.seven.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 8):
+                self.eight.clip_draw(0, 0, 13, 30, posx, self.y)
+            elif (count % 10 == 9):
+                self.nine.clip_draw(0, 0, 13, 30, posx, self.y)
+            count=count//10;
+            posx-=13
 def make_NPCblank_Init():
     rand_num = 2
     npc[0][rand_num].direct = 5
@@ -52,6 +97,7 @@ def make_NPC_New(rand_num):
         rand_num = random.randint(2, 3)
         npc[5][rand_num].direct = 5  # Blank Position
 def move_NPC():
+    cnt.update()
     for i in range(5,-1,-1):  #move NPC y Position
         for j in range(0,4):
             npc[i][j].y-=100
@@ -77,8 +123,9 @@ def Collosion():
 
 
 def enter():
-    global hero,npc,end,map
+    global hero,npc,end,map,cnt
     map=Map()
+    cnt=Count()
     end=False
     hero = Hero()
     npc = [[NPC() for i in range(4)] for i in range(6)]
@@ -109,6 +156,7 @@ def handle_events():
 
 def update():
     hero.update()
+
     if(end):
         game_framework.change_state(title_state)
 def draw():
@@ -117,7 +165,7 @@ def draw():
     map.draw()
     draw_NPC()
     hero.draw()
-
+    cnt.draw()
     #################################
     update_canvas()
     delay(0.1)
