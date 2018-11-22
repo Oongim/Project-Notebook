b3c5206e85e42f0ccc6ac6ad07603ecfd3ad4e58
Oongim-Desktop <43131702+Oongim@users.noreply.github.com>
2018-11-23 01:39:19 +0900

    결말의 맵 제작
    결말 맵을 제작함

--- Project/Map.py
+++ Project/Map.py
@@ -2,9 +2,9 @@ from pico2d import *
 
 class Map:
     def __init__(self):
-        self.y=[300,900]
+        self.y=[300,1200]
         self.position_num=3
-        self.image=load_image('Resource\\mapUP.png')
+        self.image=load_image('Resource\\Test.png')
     def update(self,move_distance):
         for i in range(2):
             self.y[i] -= move_distance
@@ -12,11 +12,11 @@ class Map:
             if self.y[i]<=-300:
                 self.y[i]=900
     def draw(self):
-        self.image.clip_draw(0, 0, 800, 600, 400, self.y[0])
-        self.image.clip_draw(0, 00, 800, 600, 400, self.y[1])
+        self.image.clip_draw(0, 0, 800, 900, 400, self.y[0])
+        self.image.clip_draw(0, 00, 800, 900, 400, self.y[1])
     def normalize_position(self):
         for i in range(2):
-            self.y[i] = (self.position_num-1+(i*6))*100
+            self.y[i] = (self.position_num-1+(i*9))*100
 
         self.position_num-=1
         if(self.position_num==-3):
